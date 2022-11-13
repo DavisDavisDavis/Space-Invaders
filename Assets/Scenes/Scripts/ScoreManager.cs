34052a40a08d214be3e2c9d12a0feff94f5c370b
Eggie <olizoooom@gmail.com>
2022-11-13 21:51:10 +0200

    Adding 3D raycast as observations

--- Assets/Scenes/Scripts/ScoreManager.cs
+++ Assets/Scenes/Scripts/ScoreManager.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Unity.MLAgents;
 
 public class ScoreManager : MonoBehaviour
 {
     public TMP_Text HighScore;
     public string HighScoreText = "HIGH SCORE:";
     int Total = 0;
+
+
 
     public void UpdateHighScore(int score)
     {
