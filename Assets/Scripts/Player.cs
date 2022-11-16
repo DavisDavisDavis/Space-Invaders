c70b335365af53c8a4733e9f1d45d0adef571f19
Eggie <olizoooom@gmail.com>
2022-11-17 01:22:44 +0200

    Game adjustments

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -43,9 +43,9 @@ public class Player : Agent
         var action = act[1];
         Position = transform.position;
 
-        if (action == 1 & Position.x > -10)
+        if (action == 1 & Position.x > -12)
             Position.x -= Time.deltaTime * Speed;
-        if (action == 2 & Position.x < 10)
+        if (action == 2 & Position.x < 12)
             Position.x += Time.deltaTime * Speed;
 
         transform.position = Position;
@@ -73,9 +73,9 @@ public class Player : Agent
     {
         var discreteActionsOut = actionsOut.DiscreteActions;
 
-        if (Input.GetKey("left") & Position.x > -10)
+        if (Input.GetKey("left") & Position.x > -12)
             discreteActionsOut[1] = 1;
-        if (Input.GetKey("right") & Position.x < 10)
+        if (Input.GetKey("right") & Position.x < 12)
             discreteActionsOut[1] = 2;
 
         if (Input.GetKey("space"))
