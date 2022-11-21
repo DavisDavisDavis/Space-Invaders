b1b44e638caba3b03eb6f53245d739984e48ffc2
Eggie <olizoooom@gmail.com>
2022-11-21 21:25:41 +0200

    Unity Settings

--- Assets/Scripts/Bullet/BulletCollision.cs
+++ Assets/Scripts/Bullet/BulletCollision.cs
@@ -12,7 +12,6 @@ public class BulletCollision : MonoBehaviour
     Player Player;
     SpawnManager spawnManager;
     int totalEnemies;
-    int enemyKillerCount = 0;
     // Start is called before the first frame update
     void Start()
     {
